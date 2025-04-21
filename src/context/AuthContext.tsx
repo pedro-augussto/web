c68676350912b80ca35b4254f7be7d157caeeabd
Pedro Augusto <pa.severosilva@gmail.com>
2025-04-21 18:11:45 -0300

    Local Storage

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -6,12 +6,17 @@ type AuthContext = {
   save: (data: UserAPIResponse) => void;
 };
 
+const LOCAL_STORAGE_KEY = "@refund"
+
 export const AuthContext = createContext({} as AuthContext);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [session, setSession] = useState<null | UserAPIResponse>(null);
 
   function save(data: UserAPIResponse) {
+    localStorage.setItem(`${LOCAL_STORAGE_KEY}:user`, JSON.stringify(data.user))
+    localStorage.setItem(`${LOCAL_STORAGE_KEY}:token`, data.token)
+
     setSession(data);
   }
 
